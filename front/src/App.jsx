e0e9102ef48085f8639a5f080e62363c559d7a55
Chapirka69 <abobapopusk5427@gmail.com>
2025-04-22 15:30:07 +0300

    booking init

---
+++ front/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from "./Layout/Layout";
+import Hero from "./components/hero";
+import About from "./components/about";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout component={Hero}/>} />
+      <Route path="/about" element={<Layout component={About}/>} />
+      <Route path='/admin' element={<Admin/>} />
+      <Route path='/Hotel/:id' element={<Layout component={Hotel}/>} />
+    </Routes>
+    </BrowserRouter> 
+  );
+}
+
+export default App;
